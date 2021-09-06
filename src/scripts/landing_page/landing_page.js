46f97ff071d665b8aaa2aa33b523e89bcfd2f69b
Your Name <Your_email>
2021-09-06 05:45:32 +0300

    landing page input dates added

--- src/scripts/landing_page/landing_page.js
+++ src/scripts/landing_page/landing_page.js
@@ -6,7 +6,6 @@ var aYear = Number(date.getFullYear());
 var aMonth = date.getMonth();
 var aDay = date.getDate();
 
-
 var datePanel = document.querySelector(".date-panel")
 var datePanelTitle = document.querySelector(".date-panel__header-title");
 var nextMonthBtn = datePanel.querySelector(".date-panel__header-btn--right");
@@ -27,7 +26,6 @@ function monthUp(){
         month = 0;
     }
     
-    datePanelTitle.textContent = `${monthList[month]} ${datePanel.dataset.year}`;
     datePanel.dataset.month = month;
     datePanel.dataset.year = year;
 
@@ -44,13 +42,14 @@ function monthDown(){
         month = 11;
     }
 
-    datePanelTitle.textContent = `${monthList[month]} ${year}`;
+   
     datePanel.dataset.month = month;
     datePanel.dataset.year = year;
 
     makeDayList();
 }
 function makeDayList(){
+    console.log("makeDayList Started")
     let oldElements = datePanel.querySelectorAll(".date-panel__days-item")
     oldElements.forEach( el => {
         el.remove();
@@ -58,18 +57,15 @@ function makeDayList(){
 
     let month = +datePanel.dataset.month;
     let year = +datePanel.dataset.year;
+    datePanelTitle.textContent = `${monthList[month]} ${year}`;
 
     datePanel.dayList = [];
 
     let firstMonthDay = new Date(year, month, 1);
     let lastMonthDay = new Date(year, month + 1, 0)
     
-    console.log("startMakeDayList ----------- ")
-    console.log(firstMonthDay.getDay() - 2);
-    console.log("");
-
     for(let i = firstMonthDay.getDay() - 2; i >= 0; i--){
-        let day = new Date(datePanel.dataset.year, datePanel.dataset.month, -i)
+        let day = new Date(year, month, -i)
         datePanel.dayList.push(day);
 
         let dayCell = document.createElement('div');
@@ -79,20 +75,35 @@ function makeDayList(){
         datePanel.querySelector(".date-panel__days").append(dayCell);
     }
     for(let i = firstMonthDay.getDate(); i < lastMonthDay.getDate() + 1; i++){
-        let day = new Date(datePanel.dataset.year, datePanel.dataset.month, i);
+        let day = new Date(year, month, i);
         datePanel.dayList.push(day);
 
         let dayCell = document.createElement('div');
         dayCell.classList.add("date-panel__days-item")
         dayCell.textContent = day.getDate();
 
+        if (datePanel.startDate){
+            if(datePanel.startDate.getDate() == day.getDate() &&
+               datePanel.startDate.getMonth() == day.getMonth() &&
+               datePanel.startDate.getFullYear() == day.getFullYear()){
+                dayCell.classList.add("date-panel__days-item--start")
+            }
+                
+        }
+        if (datePanel.endDate){
+            if (datePanel.endDate.getDate() == day.getDate() &&
+                datePanel.endDate.getMonth() == day.getMonth() &&
+                datePanel.endDate.getFullYear() == day.getFullYear()) {
+                dayCell.classList.add("date-panel__days-item--start")
+            }
+        }
+
         datePanel.querySelector(".date-panel__days").append(dayCell);
     }
     for(let i = 1; i <= 7 - lastMonthDay.getDay(); i++){
         if(lastMonthDay.getDay() === 0) break
-        console.log(i, "day", 7 - lastMonthDay.getDay());
 
-        let day = new Date(datePanel.dataset.year, datePanel.dataset.month + 1, i);
+        let day = new Date(year, month + 1, i);
         datePanel.dayList.push(day);
 
         let dayCell = document.createElement('div');
@@ -103,16 +114,16 @@ function makeDayList(){
     }
 
     
-    datePanel.dayList.forEach(element => {
-        console.log(element.getDate());
-    });
 }
 makeDayList();
 
 function dayClick(event){
     let cell = event.target.closest(".date-panel__days-item");
 
-    cell.classList.add(".data-panel__days-item--start")
+    if(datePanel.toggle)
+    cell.classList.add("date-panel__days-item--start")
+
+
 }
 
 dayClick.startDateInput = document.querySelector(".landing-page-form__date--start");
@@ -120,10 +131,76 @@ dayClick.endDateInput = document.querySelector(".landing-page-form__date--end");
 dayClick.toggle = "start";
 
 dayClick.startDateInput.addEventListener("click", () => {
-    dayClick.toggle = "start";
+    datePanel.toggle = "start";
 })
 dayClick.endDateInput.addEventListener("click", () => {
-    dayClick.toggle = "end";
+    datePanel.toggle = "end";
 })
+
+function dateInputKeyDown(event){
+    let text = this.value;
+
+    if (text.length === 2 && event.inputType !== "deleteContentBackward"){
+        text+= ".";
+        this.value = text;
+    }
+    if (text.length === 5 && event.inputType !== "deleteContentBackward"){
+        text+= ".";
+        this.value = text;
+    }
+    if(text.length > 10){
+        this.value = text.slice(0, -1);
+    }
+
+    if(text.length === 10){
+        let day = +text.slice(0, 3)
+        let month = +text.slice(3, 6)
+        let year = +text.slice(6)
+
+        if(isNaN(day) || isNaN(month) || isNaN(year)){
+            alert("невозможно преобразить в дату " + text)
+            return null
+        }
+        if(day > 31 || day < 1){
+            alert("Ошибка даты, некоректный день")
+            return null;
+        }
+        if(month > 12 || month < 1){
+            alert("Ошибка даты, некоректый месяц");
+            return null;
+        }
+        if(year > aYear + 2){
+            alert(`Ошибка даты, невозможно забронировать номер более чем на год вперед`)
+            return null;
+        }
+
+        let date = new Date(year, month - 1, day);
+        if(date < new Date()){
+            alert(`Ошибка ( Указана прошедшая дата ${text})`);
+            this.value = "";
+            return null;
+        }
+
+        console.log(datePanel.toggle)
+        if(datePanel.toggle === "start"){
+            datePanel.startDate = date;
+            datePanel.dataset.year = year
+            datePanel.dataset.month = month - 1;
+        }else if(datePanel.toggle === "end"){
+            console.log("dateEnd")
+            datePanel.endDate = date;
+            datePanel.dataset.year = year
+            datePanel.dataset.month = month - 1;
+        }
+        makeDayList();
+
+
+    }
+
+
+}
+
+dayClick.startDateInput.addEventListener("input", dateInputKeyDown);
+dayClick.endDateInput.addEventListener("input", dateInputKeyDown);
 
 document.querySelector('.date-panel__days').addEventListener("click", dayClick)
